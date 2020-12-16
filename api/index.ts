fec429b70b766d631a4def5d2cbb6997ef186798
David Siegel <djsiegel@gmail.com>
2020-12-16 11:46:09 -0600

    Enrich more

--- api/index.ts
+++ api/index.ts
@@ -16,54 +16,63 @@ function addCORS(response) {
   response.set("Access-Control-Allow-Headers", "Content-Type");
 }
 
-// This answers the OPTIONS request, which is used for CORS.
-app.options("/api/full-name", async (request, response) => {
-  addCORS(response);
-  return response.status(204).send("");
-});
+interface Enrichement {
+  person: any;
+  company: any;
+}
 
-// This is the actual API request
-app.post("/api/full-name", async (request, response) => {
-  addCORS(response);
+function enrichment(path: string, t: (enrichment: Enrichement) => string) {
+  const apiPath = `/api${path}`;
 
-  console.log(request.body);
+  // This answers the OPTIONS request, which is used for CORS.
+  app.options(apiPath, async (request, response) => {
+    addCORS(response);
+    return response.status(204).send("");
+  });
 
-  // The parameters passed from Glide are in the `params`
-  // field of the request body.  Our request takes a single
-  // parameter, which is called `pokemon`.
-  const {
-    params: { email },
-  } = request.body;
+  // This is the actual API request
+  app.post(apiPath, async (request, response) => {
+    addCORS(response);
 
-  if (
-    email === undefined ||
-    email.type !== "string" ||
-    !validateEmail(email.value)
-  ) {
-    return response.sendStatus(400);
-  }
+    console.log(request.body);
 
-  let enrich;
-  try {
-    enrich = await clearbit.Enrichment.find({
-      email: email.value,
-      stream: true,
-    });
-  } catch (error) {
-    console.error(error);
-    return response.sendStatus(400);
-  }
+    // The parameters passed from Glide are in the `params`
+    // field of the request body.  Our request takes a single
+    // parameter, which is called `pokemon`.
+    const {
+      params: { email },
+    } = request.body;
 
-  const { person, company } = enrich;
-  if (person === undefined || company === undefined) {
-    return response.sendStatus(400);
-  }
+    if (
+      email === undefined ||
+      email.type !== "string" ||
+      !validateEmail(email.value)
+    ) {
+      return response.sendStatus(400);
+    }
 
-  response.type("application/json");
+    let enrich;
+    try {
+      enrich = await clearbit.Enrichment.find({
+        email: email.value,
+        stream: true,
+      });
+    } catch (error) {
+      console.error(error);
+      return response.sendStatus(400);
+    }
 
-  return response.send(
-    JSON.stringify({ type: "string", value: person.name.fullName })
-  );
-});
+    if (enrich?.person === undefined || enrich?.company === undefined) {
+      return response.sendStatus(400);
+    }
+
+    response.type("application/json");
+
+    return response.send(JSON.stringify({ type: "string", value: t(enrich) }));
+  });
+}
+
+enrichment("/full-name", (enrich) => enrich.person.name.fullName);
+enrichment("/avatar", (enrich) => enrich.person.avatar);
 
 export default app;
